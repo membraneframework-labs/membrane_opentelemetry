f121303735cb56d793fc18116c1dc4dbc813d82c
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-06-07 15:51:56 +0200

    Add new macros

--- lib/opentelemetry.ex
+++ lib/opentelemetry.ex
@@ -18,6 +18,36 @@ defmodule Membrane.OpenTelemetry do
       else: default_macro([name, timestamp])
   end
 
+  defmacro set_current_span(name) do
+    if @enabled,
+      do: do_set_current_span(name),
+      else: default_macro([name])
+  end
+
+  defmacro set_attribute(name, key, value) do
+    if @enabled,
+      do: do_set_attribute(name, key, value),
+      else: default_macro([name, key, value])
+  end
+
+  defmacro set_attributes(name, attributes) do
+    if @enabled,
+      do: do_set_attributes(name, attributes),
+      else: default_macro([name, attributes])
+  end
+
+  defmacro add_event(name, event, attributes) do
+    if @enabled,
+      do: do_add_event(name, event, attributes),
+      else: default_macro([name, event, attributes])
+  end
+
+  defmacro add_events(name, events) do
+    if @enabled,
+      do: do_add_events(name, events),
+      else: default_macro([name, events])
+  end
+
   defp do_start_span(name, opts) do
     quote do
       with %{parent: parent_name} when parent <- unquote(opts) do
@@ -27,6 +57,7 @@ defmodule Membrane.OpenTelemetry do
 
       new_span = OpenTelemetry.Tracer.start_span(name)
       unquote(__MODULE__).store_span(unquote(name), span)
+      OpenTelemetry.Tracer.set_current_span(new_span)
     end
   end
 
@@ -36,6 +67,53 @@ defmodule Membrane.OpenTelemetry do
       |> OpenTelemetry.Tracer.set_current_span()
 
       OpenTelemetry.Tracer.end_span(unquote(timestamp))
+    end
+  end
+
+  defp do_set_current_span(name) do
+    quote do
+      unquote(__MODULE__).get_span(unquote(name))
+      |> OpenTelemetry.Tracer.set_current_span()
+    end
+  end
+
+  defp do_set_attribute(name, key, value) do
+    call_with_current_span(
+      name,
+      fn -> OpenTelemetry.Tracer.set_attribute(key, value) end
+    )
+  end
+
+  defp do_set_attributes(name, attributes) do
+    call_with_current_span(
+      name,
+      fn -> OpenTelemetry.Tracer.set_attributes(attributes) end
+    )
+  end
+
+  defp do_add_event(name, event, attributes) do
+    call_with_current_span(
+      name,
+      fn -> OpenTelemetry.Tracer.add_event(event, attributes) end
+    )
+  end
+
+  defp do_add_events(name, events) do
+    call_with_current_span(
+      name,
+      fn -> OpenTelemetry.Tracer.add_events(events) end
+    )
+  end
+
+  defp call_with_current_span(name, function) do
+    quote do
+      old_current_span = OpenTelemetry.Tracer.current_span_ctx()
+
+      span = unquote(__MODULE__).get_span(unquote(name))
+      OpenTelemetry.Tracer.set_current_span(span)
+      unquote(function).()
+
+      OpenTelemetry.Tracer.set_current_span(old_current_span)
     end
   end
 
